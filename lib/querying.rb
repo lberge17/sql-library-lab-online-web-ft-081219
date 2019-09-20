9e28572ce2d31b56bb3498c7e56a835fd23f826e
Laura Berge <lauraeberge@gmail.com>
2019-09-20 19:47:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,8 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.author_id = characters.author_id 
-  GROUP BY series.title HAVING characters.species = 'human'
+  GROUP BY series.title 
+  HAVING characters.species = 'human'
   ORDER BY COUNT(species) DESC
   LIMIT 1;"
 end
